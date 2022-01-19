3a278f814e8ae2849753c2d80a852ffb348d25b1
beltrancruz <beltrancruzdaniel@gmail.com>
2022-01-18 20:54:59 -0600

    Final Section 9 - Countries App

--- src/app/country/pages/by-capital/by-capital.component.ts
+++ src/app/country/pages/by-capital/by-capital.component.ts
@@ -5,7 +5,11 @@ import { Country } from '../../interfaces/country.interface';
 @Component({
   selector: 'app-by-capital',
   templateUrl: './by-capital.component.html',
-  styles: [
+  styles: [`
+    li{
+      cursor:pointer;
+    }
+  `
   ]
 })
 export class ByCapitalComponent {
@@ -16,14 +20,34 @@ export class ByCapitalComponent {
   q: string = "";
   isErr: boolean = false;
   countries: Country[] = [];
+  countriesSuggested: Country[] = [];
+  word: string = "";
+  showSuggestions: boolean = false;
 
   suggestion(event:string):void{
+    if(event.trim().length === 0){
+      this.showSuggestions = false;
+      return;
+    }
+    this.showSuggestions = true;
+    this.word = event;
     this.isErr = false;
     console.log('suggestion: ',event);
+    this._countryService.searchCountryByCapital(event).subscribe(countries => {
+      try {
+        this.countriesSuggested = countries.splice(0,5);  
+      } catch (error) {
+        this.countriesSuggested = [];  
+      }
+      
+    }, (err)=>{
+      this.countriesSuggested = [];
+    })
     
   }
   
   search(q:string):void{
+    this.showSuggestions = false;
     this.q = q;
     this.isErr = false;
 
@@ -41,6 +65,11 @@ export class ByCapitalComponent {
       
     })
     
+    
+  }
+
+  searchWord(q:string):void{
+    this.search(q);
     
   }
 
